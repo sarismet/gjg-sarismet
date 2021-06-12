8bc50cd45fb33f7a49d0b0a0cfdb02060f91b0f9
sarismet <sarismet2825@gmail.com>
2021-06-12 20:07:37 +0300

    the issue that not creating a test db is solved

--- tests/get_leaderboard_test.go
+++ tests/get_leaderboard_test.go
@@ -34,17 +34,17 @@ func TestGetLeaderBoard(t *testing.T) {
 		Client: newRedisClient,
 	}
 	const (
-		host         = "localhost"
+		host         = "0.0.0.0"
 		port         = 5432
 		databaseuser = "postgres"
 		password     = "123"
+		dbname       = "postgres"
 	)
+	mainDBconnection := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, databaseuser, password, dbname)
 
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-		"password=%s sslmode=disable",
-		host, port, databaseuser, password)
-
-	sqldb, err := sql.Open("postgres", psqlInfo)
+	sqldb, err := sql.Open("postgres", mainDBconnection)
 
 	if err != nil {
 		log.Fatal(err)
@@ -53,12 +53,18 @@ func TestGetLeaderBoard(t *testing.T) {
 	dbName := "testdb"
 	_, err = sqldb.Exec("create database " + dbName + ";")
 	if err != nil {
-		//handle the error
 		log.Fatal(err)
 	}
 
+	testDBconnection := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, databaseuser, password, dbName)
+	testdb, err := sql.Open("postgres", testDBconnection)
+	if err != nil {
+		log.Fatal(err)
+	}
 	SQLDB := &db.SQLDatabase{
-		SqlClient: sqldb,
+		SqlClient: testdb,
 	}
 
 	app.RedisDB = RedisDB
@@ -83,13 +89,14 @@ func TestGetLeaderBoard(t *testing.T) {
 		assert.Equal(t, "ismet", users[0].Display_Name)
 	}
 
-	userJSON = fmt.Sprintf(`{"score_worth":100,"user_id":"%s"}`, users[0].User_Id)
+	userJSON = fmt.Sprintf(`{"score_worth":100,"user_id":"%s"}`, users[1].User_Id)
 	req = httptest.NewRequest(http.MethodPost, "/", strings.NewReader(userJSON))
 	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
 	rec = httptest.NewRecorder()
 	c = e.NewContext(req, rec)
 	if assert.NoError(t, app.ScoreSubmit(c)) {
 		var responseScore db.Score
+		fmt.Println("rec.Body.Bytes() is ", rec.Body.String())
 		json.Unmarshal(rec.Body.Bytes(), &responseScore)
 		assert.Equal(t, http.StatusOK, rec.Code)
 		assert.Equal(t, responseScore.Score_worth, float64(100))
@@ -107,10 +114,15 @@ func TestGetLeaderBoard(t *testing.T) {
 		assert.Equal(t, responseUsers[0].Display_Name, users[0].Display_Name)
 		assert.Equal(t, responseUsers[1].Display_Name, users[1].Display_Name)
 	}
+	app.SQLDB.SqlClient.Close()
 
+	sqldb, err = sql.Open("postgres", mainDBconnection)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 	_, err = sqldb.Exec("DROP DATABASE IF EXISTS " + dbName + ";")
 	if err != nil {
-		//handle the error
 		log.Fatal(err)
 	}
 }
